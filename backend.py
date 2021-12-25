ea8a5876239f1c51b3302ed47fdd0664b19677ca
Quan <cloudmadeofcandy@gmail.com>
2021-12-26 00:18:53 +0700

    nothing much

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from AES import *
+import time
 app = Flask(__name__, static_folder="static")
 @app.route('/')
 def hello_world():
@@ -8,16 +9,20 @@ def hello_world():
 @app.route('/encrypt', methods = ['POST'])
 def Aencrypt():
    info = ("state", "key", "IV", "cipher", "hexain", "hexakey", "hexaout", "hexaIV", "b64", "mode")
-   d = {i : request.form.get(i, "False") for i in info}  
-   e = encrypt(**d) 
-   return {"cypherkey": e}
+   d = {i : request.form.get(i, "False") for i in info}
+   rtime = time.process_time() 
+   e = encrypt(**d)
+   rtime = str((time.process_time() - rtime)*1000) + "ms"
+   return {"cypherkey": e, "time": rtime}
 
 @app.route('/decrypt', methods = ['POST'])
 def Adecrypt():
    info = ("state", "key", "IV", "cipher", "hexain", "hexakey", "hexaout", "hexaIV", "b64", "mode")
    d = {i : request.form.get(i, "False") for i in info}  
+   rtime = time.process_time()
    e = decrypt(**d)
-   return {"cypherkey": e}
+   rtime = str((time.process_time() - rtime)*1000) + "ms"
+   return {"cypherkey": e, "time": rtime}
 
 if __name__ == '__main__':
    app.run()
